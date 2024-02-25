91e0dcce9c6b15df133e1fb4380fc9c9ca79afec
Sherryz707 <101007005+Sherryz707@users.noreply.github.com>
2024-02-26 00:44:01 +0500

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Providers } from "./context/Providers";
 import { getSettings } from "@/sanity/lib/client";
 import { urlForImage } from "@/sanity/lib/image";
 import { Organization, WithContext } from "schema-dts";
-
+import Script from 'next/script';
 const inter = Inter({ subsets: ["latin"] });
 
 
@@ -23,6 +23,16 @@ export default async function RootLayout({
           <main className="flex min-h-screen flex-col w-full">{children}</main>
         </Providers>
       </body>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.AnalyticsId || ""}`}
+      />
+      <Script id='gtag'>
+      {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${process.env.AnalyticsId || ""});`}
+    </Script>
     </html>
   );
 }
